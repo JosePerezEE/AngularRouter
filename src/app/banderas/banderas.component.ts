9d866e9fb3afeb4d280939a8193668d5a6a7881c
Carmen <heytureyes@gmail.com>
2019-11-16 19:15:01 -0600

    Cambio en banderas e informacion contacto

---
+++ src/app/banderas/banderas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banderas',
+  templateUrl: './banderas.component.html',
+  styleUrls: ['./banderas.component.css']
+})
+export class BanderasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
